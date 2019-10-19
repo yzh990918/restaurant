ff04073686fd13f2567853e473495e2764e09a4f
251205668@qq.com <251205668@qq.com>
2019-10-19 23:48:41 +0800

    安装cube-ui以及获取mock数据实现

--- restaurant/src/main.js
+++ restaurant/src/main.js
@@ -2,7 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-
+import "./assets/styles/reset.css";
+import "./assets/styles/border.css";
+import fastClick from "fastclick";
+import "./cube-ui";
+fastClick.attach(document.body);
 Vue.config.productionTip = false;
 
 new Vue({
---
+++ restaurant/vue.config.js
@@ -0,0 +1,42 @@
+const appData = require("./data.json");
+const seller = appData.seller;
+const goods = appData.goods;
+const ratings = appData.ratings;
+module.exports = {
+  css: {
+    loaderOptions: {
+      stylus: {
+        "resolve url": true,
+        import: ["./src/theme"]
+      }
+    }
+  },
+  pluginOptions: {
+    "cube-ui": {
+      postCompile: true,
+      theme: true
+    }
+  },
+  devServer: {
+    before(app) {
+      app.get("/api/seller", function(req, res) {
+        res.json({
+          errno: 0,
+          data: seller
+        });
+      });
+      app.get("/api/goods", function(req, res) {
+        res.json({
+          errno: 0,
+          data: goods
+        });
+      });
+      app.get("/api/ratings", function(req, res) {
+        res.json({
+          errno: 0,
+          data: ratings
+        });
+      });
+    }
+  }
+};
