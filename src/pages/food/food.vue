73103e5d95651c4992ea5c2820585bcbb00c789d
251205668@qq.com <251205668@qq.com>
2019-11-03 18:16:42 +0800

    商品信息展示 分割区使用

--- src/pages/food/food.vue
+++ src/pages/food/food.vue
@@ -1,6 +1,6 @@
 <template>
   <transition name="move">
-    <div v-show="showFlag" class="food">
+    <div ref="food" v-show="showFlag" class="food">
       <div class="food-content">
         <div class="image-header">
           <img :src="food.image" />
@@ -10,23 +10,44 @@
         </div>
         <div class="content">
           <h1 class="title">{{ food.name }}</h1>
-          <span class="detail">
+          <div class="detail">
             <span class="sell-count">月售{{ food.count }}份</span>
             <span class="rating">好评率{{ food.rating }}</span>
-          </span>
+          </div>
           <div class="price">
             <span class="now">￥{{ food.price }}</span
             ><span class="old" v-show="food.oldPrice"
               >￥{{ food.oldPrice }}</span
             >
           </div>
+          <div class="carcontrol-wrapper">
+            <carcontrol :food="food"></carcontrol>
+          </div>
+          <transition name="fade">
+            <div
+              @click="addFirst"
+              class="buy"
+              v-show="!food.count || food.count == 0"
+            >
+              加入购物车
+            </div>
+          </transition>
         </div>
+      </div>
+      <split v-show="food.info"></split>
+      <div class="info" v-show="food.info">
+        <h1 class="title">商品信息</h1>
+        <p class="text">{{ food.info }}</p>
       </div>
     </div>
   </transition>
 </template>
 
 <script>
+import BScorll from "better-scroll";
+import carcontrol from "../carcontrol/carcontrol";
+import split from "../split/split";
+import Vue from "vue";
 export default {
   name: "food",
   props: {
@@ -40,7 +61,10 @@ export default {
     };
   },
 
-  components: {},
+  components: {
+    carcontrol,
+    split
+  },
 
   computed: {},
 
@@ -51,9 +75,24 @@ export default {
   methods: {
     show() {
       this.showFlag = true;
+      this.$nextTick(() => {
+        if (!this.scroll) {
+          this.scroll = new BScorll(this.$refs.food, {
+            click: true
+          });
+        } else {
+          this.scroll.refresh();
+        }
+      });
     },
     hide() {
       this.showFlag = false;
+    },
+    addFirst(e) {
+      window.console.log("click");
+      // 设置count为1
+      Vue.set(this.food, "count", 1);
+      this.$emit("addcar", e.target);
     }
   },
 
@@ -95,4 +134,68 @@ export default {
         padding: 10px
         font-size: 20px
         color: #fff
+  .content
+    position: relative
+    padding: 18px
+    .title
+      line-height: 14px
+      margin-bottom: 8px
+      font-size: 14px
+      font-weight: 700
+      color: rgb(7, 17, 27)
+    .detail
+      margin-bottom: 18px
+      line-height: 10px
+      height: 10px
+      font-size: 0
+      .sell-count, .rating
+        font-size: 10px
+        color: rgb(147, 153, 159)
+      .sell-count
+        margin-right: 12px
+    .price
+      font-weight: 700
+      line-height: 24px
+      .now
+        margin-right: 8px
+        font-size: 14px
+        color: rgb(240, 20, 20)
+      .old
+        text-decoration: line-through
+        font-size: 10px
+        color: rgb(147, 153, 159)
+    .carcontrol-wrapper
+      position: absolute
+      right: 12px
+      bottom: 12px
+    .buy
+      position: absolute
+      right: 18px
+      bottom: 18px
+      z-index: 10
+      height: 24px
+      line-height: 24px
+      padding: 0 12px
+      box-sizing: border-box
+      font-size: 10px
+      border-radius: 12px
+      color: #fff
+      background-color: rgb(0, 160, 220)
+      opacity: 1
+      transition: all 0.2s
+      &.fade-enter, &.fade-leave-active
+        opacity: 0
+  .info
+    padding: 18px
+    .title
+      line-height: 14px
+      margin-bottom: 16px
+      font-size: 14px
+      color: rgb(7, 17, 27)
+      font-weight: 700
+    .text
+      line-height: 24px
+      font-size: 12px
+      padding: 0 8px
+      color: rgb(77, 85, 93)
 </style>
---
+++ src/pages/split/split.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="split"></div>
+</template>
+
+<script>
+export default {
+  name: "split",
+  props: [""],
+  data() {
+    return {};
+  },
+
+  components: {},
+
+  computed: {},
+
+  beforeMount() {},
+
+  mounted() {},
+
+  methods: {},
+
+  watch: {}
+};
+</script>
+<style lang="stylus" scoped>
+.split
+  width: 100%
+  height: 16px
+  border-top: 1px solid rgba(7, 17, 27, 0.1)
+  border-bottom: 1px solid rgba(7, 17, 27, 0.1)
+  background: #f3f5f7
+</style>
